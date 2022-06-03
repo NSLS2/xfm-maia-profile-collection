96b9b8f51c87f8b9ac8baf8c05f9f04cd0be320d
Ji Li <liji@bnl.gov>
2022-06-03 15:51:13 -0400

    MNT: switch stage PVs

--- startup/10-stages.py
+++ startup/10-stages.py
@@ -2,14 +2,9 @@ from ophyd import EpicsMotor, Device, Component as Cpt
 
 
 class MaiaStage(Device):
-    x = Cpt(EpicsMotor, "{PI180:1-Ax:MaiaX}Mtr")
-    y = Cpt(EpicsMotor, "{PI180:1-Ax:MaiaY}Mtr")
-    z = Cpt(EpicsMotor, "{PI180:1-Ax:MaiaZ}Mtr")
-    r = Cpt(EpicsMotor, "{SR50pp:1-Ax:MaiaR}Mtr")
+    x    = Cpt(EpicsMotor, '{X12:1-Ax:X}Mtr')
+    y    = Cpt(EpicsMotor, '{X12:1-Ax:Y}Mtr')
 
-
-M = MaiaStage("XF:04BMC-ES:2", name="M")
-M_x = M.x
-M_y = M.y
-M_z = M.z
-M_r = M.r
+M = MaiaStage('XF:04BM-ES:2', name='M')
+M_x   = M.x
+M_y   = M.y
