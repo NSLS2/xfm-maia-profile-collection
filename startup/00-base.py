c3b416670e106019cf35d9fde562257a008bf1c6
XFM Operator <xf04bm@xf04bm-ws2.nsls2.bnl.local>
2025-05-07 15:41:51 -0400

    add config for RedisJsonDict for metadata
     * also restore olog initialization

--- startup/00-base.py
+++ startup/00-base.py
@@ -8,12 +8,21 @@ nslsii.configure_base(
   get_ipython().user_ns, 
   'xfm',
   publish_documents_with_kafka=True
-)
 
-#nslsii.configure_olog(get_ipython().user_ns)
+  )
+import redis
+from redis_json_dict import RedisJSONDict
+
+uri = "info.xfm.nsls2.bnl.gov"
+# Provide an endstation prefix, if needed, with a trailing "-"
+new_md = RedisJSONDict(redis.Redis(uri), prefix="maia")
+#BEAMLINE_ID = 'xfm'
+
+nslsii.configure_olog(get_ipython().user_ns)
 
 #Optional: set any metadata that rarely changes.
-RE.md['beamline_id'] = 'XFM'
+#RE.md['beamline_id'] = 'XFM'
+RE.md = new_md
 
 #beam_current = EpicsSignal('XF:04BM-ES:2{Sclr:1}scaler1.s4')
 beam_current = EpicsSignal('SR:OPS-BI{DCCT:1}I:Real-I')
